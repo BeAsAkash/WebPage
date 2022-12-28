d6e28ba92c10d65a09e1b3e13d43440ecdaa0925
BeAsAkash <kumaraadarsh8318@gmail.com>
2022-12-28 08:50:15 +0530

    Added more  visuals.

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,47 +162,10 @@
   });
 
   
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  
 
   
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
+  
   
   window.addEventListener('load', () => {
     AOS.init({
